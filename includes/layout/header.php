282abb7e6934008254654479a3c030f86928d8d0
DanielLaborda <dani.laborda90@gmail.com>
2021-11-09 00:35:25 +0100

    Inicio del ejercicio

---
+++ includes/layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Birthstone+Bounce:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php 
+     include 'includes/layout/header.php';
+?>
+
+<div>
+    Hola    
+</div>
+
+<?php include 'includes/layout/footer.php'; ?>
